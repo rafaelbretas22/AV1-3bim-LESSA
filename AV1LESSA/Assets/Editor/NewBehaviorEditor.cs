ff02b583628c3bf390361f6f58811a2089c88faf
Rafael Bretas <rafael.bretas2000@gmail.com>
2017-08-18 10:47:42 -0300

    Add files via upload

---
+++ AV1LESSA/Assets/Editor/NewBehaviorEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NewBehaviourScript))]
+public class NewBehaviorEditor : Editor {
+
+
+	 public override void OnInspectorGUI(){
+	
+		NewBehaviourScript Behaviour = target as NewBehaviourScript;
+
+		foreach(Entry e in Behaviour.lista){
+			
+			e.value = EditorGUILayout.IntField (e.key, e.value );
+			
+		
+		}
+	}
+}
---
+++ AV1LESSA/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entry 
+{
+	public string key;
+	public int value;
+
+	public Entry (string k, int v)
+	{
+		key = k;
+		value = v;
+	}
+}
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public List<Entry> lista = new List<Entry>();
+
+	void Reset()
+	{
+		lista.Add (new Entry ("kilos", 0));
+	}
+
+
+}
